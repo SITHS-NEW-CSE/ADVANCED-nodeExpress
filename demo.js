2c405ac31acdf7e4ac1fc3269607ab3c66d032aa
Richard So <richardso2021@gmail.com>
2021-07-29 10:58:36 -0400

    node + express

---
+++ demo.js
@@ -0,0 +1,8 @@
+// you can run JS files on your terminal directly (without a browser)
+// Node.JS is a JS engine installed on your computer which executes JS code 
+// browsers have different types of JS engines too (Chrome: V8, Firefox: SpiderMonkey)
+
+// run this file using `node demo.js`
+const x = 10;
+const y = 20;
+console.log(x + y);
---
+++ readme.md
@@ -0,0 +1,7 @@
+## NPM Dependencies
+ - express
+ - nodemon
+Install the above packages using this command:
+```
+npm i express nodemon
+```
